e746b892fd6b6311aac94f00158c2ab2c74d661c
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-01-16 10:47:44 +0200

    0x02

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,15 +4,16 @@
 
 
 def minOperations(n):
-  '''Minimum Operations
-  '''
-  if (n < 2):
-      return 0
-  ops, root = 0, 2
-  while root <= n:
-      if n % root == 0:
-          ops += root
-          n = n / root
-          root -= 1
-      root += 1
-  return ops
+    '''
+    Minimum Operations
+    '''
+    if (n < 2):
+        return 0
+    ops, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
+    return ops
