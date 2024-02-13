80ee2ca0a91d4a982224956bd544d40107369f47
Abdulrahman Moussa <abdelrahmanmoussa111@gmail.com>
2024-02-13 05:22:23 -0800

    0x06

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,14 @@
 const request = require('request');
 const apiURL = "https://swapi-api.hbtn.io/api/films/"
 
-request(`${apiURL}${process.argv[2]}`, (err, res, body) => {
+request(`${apiURL}${process.argv[2]}`, function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-  request(actors[x], (err, res, body) => {
+  request(actors[x], function (err, res, body) {
     if (err) throw err;
     console.log(JSON.parse(body).name);
     exactOrder(actors, x + 1);
